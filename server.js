867a11b0c58d19d850f06ac3ab673fe33c84c2bc
sssssqew <sunrise.in.my.dream@gmail.com>
2025-03-28 20:56:53 +0700

    eliminated unnecessary codes

--- server.js
+++ server.js
@@ -22,9 +22,7 @@
     idleTimeout: 255, // 4min. 25sec
     development: true,
     reusePort: true, // Share the same port across multiple processes (only works on linux os)
-    // static: {
-    //   "/csr" : () => new Response(homepage, { headers: { "Content-Type": "text/html" }})
-    // },
+
     routes: {
       "/":  async (req, server) => {
         logging(req, server)
@@ -51,7 +49,6 @@
 
     // Fallback handler
     // async fetch(request, server) {  
-      // const url = new URL(request.url);  
       // server.timeout(request, 60); // Set 60 second timeout for this request
       // await request.text(); // If they take longer than 60 seconds to send the body, the request will be aborted
       // if(url.pathname === '/'){
